3e8b5f442bb13bda5a5b67f5730cf37cbf8b9701
Kurz <kurz956kk@gmail.com>
2023-12-16 21:59:30 +0500

    a few data changes

--- book_app/models.py
+++ book_app/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)     # поле типа CharField с максимальной длиной значения 70 символов
     rating = models.IntegerField()      # рейтинг книги, поле типа IntegerField
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return f'{self.title} - {self.rating}'
+        return f'{self.title} - {self.rating} - {self.author}'
+
+
+
+
+#  from book_app.models import Book
